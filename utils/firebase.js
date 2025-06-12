b2cf528edf3216bd6cc031cb43acec2a5151ad6f
Julien-Boum <julienjarry@orange.fr>
2025-06-12 22:33:55 +0200

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,17 +1,8 @@
-// /utils/firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB7rbXuhC0rbMW8yK24sX9Rx9Qvinuwb0",
-  authDomain: "config-444cb.firebaseapp.com",
-  projectId: "config-444cb",
-  storageBucket: "config-444cb.appspot.com",
-  messagingSenderId: "240337773323",
-  appId: "1:240337773323:web:4843d915ef11b77e2f80e"
+  apiKey: "xxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxx.firebaseapp.com",
+  projectId: "xxxxxxxx",
+  storageBucket: "xxxxxxxx.appspot.com",
+  messagingSenderId: "xxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxx"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
