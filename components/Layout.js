ff5bbc100dd1cbd6f5ce1a35364ecf195f9d4632
Julien-Boum <julienjarry@orange.fr>
2025-06-12 07:10:37 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,39 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import fetchConfig from '../utils/fetchConfig'
+import React, { useContext } from 'react';
+import { AppContext } from '../utils/AppContext';
+import SocialLinks from './SocialLinks';
+import BuyButton from './BuyButton';
 
 const Layout = ({ children }) => {
-  const [config, setConfig] = useState(null)
+  const { config } = useContext(AppContext);
 
-  useEffect(() => {
-    const loadConfig = async () => {
-      const data = await fetchConfig()
-      setConfig(data)
-    }
-    loadConfig()
-  }, [])
-
-  if (!config) {
-    return <div>Chargement en cours...</div>
-  }
-
-  const { backgroundUrl, logoUrl } = config
+  const backgroundStyle = {
+    backgroundImage: config.backgroundUrl ? `url(${config.backgroundUrl})` : 'none',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    color: '#fff',
+    padding: '2rem',
+  };
 
   return (
-    <div
-      style={{
-        backgroundImage: `url(${backgroundUrl})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        minHeight: '100vh',
-        padding: '20px'
-      }}
-    >
-      <header style={{ textAlign: 'center', marginBottom: '40px' }}>
-        {logoUrl && <img src={logoUrl} alt="Boumcoin Logo" style={{ height: '100px' }} />}
-      </header>
-      <main>{children}</main>
-    </div>
-  )
-}
+    <div style={backgroundStyle}>
+      {config.logoUrl && (
+        <img
+          src={config.logoUrl}
+          alt="Logo Boumcoin"
+          style={{ width: '150px', marginBottom: '1rem' }}
+        />
+      )}
 
-export default Layout
+      {config.title && <h1>{config.title}</h1>}
+      {config.description && <p>{config.description}</p>}
+
+      <BuyButton />
+
+      <SocialLinks links={config.socials} />
+
+      <div style={{ marginTop: '2rem' }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
