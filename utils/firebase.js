8b7f88c5ee11965fdaa0744b280a28485ac8253e
Julien-Boum <julienjarry@orange.fr>
2025-06-12 06:38:39 +0200

    Create firebase.js

---
+++ utils/firebase.js
@@ -0,0 +1,26 @@
+// utils/firebase.js
+import { initializeApp } from 'firebase/app'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export const fetchConfig = async () => {
+  const ref = doc(db, 'config', 'main')
+  const snap = await getDoc(ref)
+  if (snap.exists()) {
+    return snap.data()
+  } else {
+    console.warn('Configuration Firebase introuvable.')
+    return {}
+  }
+}
