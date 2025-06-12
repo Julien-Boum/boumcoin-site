4402287f36f5d50317f562e6bea6d45f0520d482
Julien-Boum <julienjarry@orange.fr>
2025-06-12 07:09:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
+// pages/index.js
+
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
+import { fetchConfig } from '../utils/fetchConfig'
+import { AppContext } from '../utils/AppContext'
+import Layout from '../components/Layout'
 import BuyButton from '../components/BuyButton'
+import SocialLinks from '../components/SocialLinks'
 
 export default function Home() {
+  const [config, setConfig] = useState(null)
+
+  useEffect(() => {
+    fetchConfig().then(setConfig)
+  }, [])
+
+  if (!config) return <p>Chargement...</p>
+
   return (
-    <div>
-      {/* ... autres composants */}
-      <BuyButton />
-    </div>
-  );
+    <>
+      <Head>
+        <title>{config?.texts?.fr?.title || 'BOUMCOIN'}</title>
+      </Head>
+      <AppContext.Provider value={config}>
+        <Layout>
+          <h1>{config?.texts?.fr?.title}</h1>
+          <p>{config?.texts?.fr?.description}</p>
+          <BuyButton />
+          <SocialLinks />
+        </Layout>
+      </AppContext.Provider>
+    </>
+  )
 }
