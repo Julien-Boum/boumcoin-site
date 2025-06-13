b0f6af2aeb5e08e89479737977d44d6c5cc53c86
Julien-Boum <julienjarry@orange.fr>
2025-06-13 08:07:28 +0200

    Update _apps.js

--- pages/_apps.js
+++ pages/_apps.js
@@ -1,24 +1,24 @@
-import '../styles/globals.css'
-import { useEffect, useState } from 'react'
-import { fetchConfig } from '../utils/firebase'
-import AppContext from '../utils/AppContext'
+import '../styles/globals.css';
+import { useEffect, useState } from 'react';
+import fetchConfig from '../utils/fetchConfig';
+import AppContext from '../utils/AppContext';
 
 export default function MyApp({ Component, pageProps }) {
-  const [config, setConfig] = useState(null)
+  const [config, setConfig] = useState(null);
 
   useEffect(() => {
-    const loadConfig = async () => {
-      const data = await fetchConfig()
-      setConfig(data)
-    }
-    loadConfig()
-  }, [])
+    const load = async () => {
+      const data = await fetchConfig();
+      setConfig(data);
+    };
+    load();
+  }, []);
 
-  if (!config) return <div>Chargement...</div>
+  if (!config) return <div>Chargement...</div>;
 
   return (
     <AppContext.Provider value={config}>
       <Component {...pageProps} />
     </AppContext.Provider>
-  )
+  );
 }
