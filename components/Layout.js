9ee86978682b5c9689520576eefb025c15bb9aa6
Julien-Boum <julienjarry@orange.fr>
2025-06-13 08:04:59 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,40 +1,31 @@
 import React, { useContext } from 'react';
-import { AppContext } from '../utils/AppContext';
-import SocialLinks from './SocialLinks';
+import AppContext from '../utils/AppContext';
 import BuyButton from './BuyButton';
+import ContactForm from './ContactForm';
+import SocialLinks from './SocialLinks';
 
 const Layout = ({ children }) => {
-  const { config } = useContext(AppContext);
+  const config = useContext(AppContext);
 
   const backgroundStyle = {
-    backgroundImage: config.backgroundUrl ? `url(${config.backgroundUrl})` : 'none',
+    backgroundImage: config?.backgroundUrl ? `url(${config.backgroundUrl})` : 'none',
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     minHeight: '100vh',
-    color: '#fff',
-    padding: '2rem',
+    color: 'white',
+    padding: '2rem'
   };
 
   return (
     <div style={backgroundStyle}>
-      {config.logoUrl && (
-        <img
-          src={config.logoUrl}
-          alt="Logo Boumcoin"
-          style={{ width: '150px', marginBottom: '1rem' }}
-        />
-      )}
-
-      {config.title && <h1>{config.title}</h1>}
-      {config.description && <p>{config.description}</p>}
+      {config?.logoUrl && <img src={config.logoUrl} alt="Logo Boumcoin" style={{ width: '150px', marginBottom: '1rem' }} />}
+      {config?.title && <h1>{config.title}</h1>}
+      {config?.description && <p>{config.description}</p>}
 
       <BuyButton />
+      <SocialLinks links={config?.socials} />
 
-      <SocialLinks links={config.socials} />
-
-      <div style={{ marginTop: '2rem' }}>
-        {children}
-      </div>
+      <div style={{ marginTop: '2rem' }}>{children}</div>
     </div>
   );
 };
